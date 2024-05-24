4f7cd7fbe3068f7a80835f5d2bef1301da0142b1
juliachelsie Gustafsson <juliachelsiegustafsson@gmail.com>
2024-05-24 11:54:10 +0000

    Added templates and views for product

--- home/views.py
+++ home/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    """ A view to return index.html """
     return render(request, 'home/index.html')
--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def every_product(request):
+    """ A view to show all the products """
+
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
