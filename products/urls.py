bc1eb1e066f49d9e08beeba89da5617fc7e6e422
juliachelsie Gustafsson <juliachelsiegustafsson@gmail.com>
2024-05-31 14:36:44 +0000

    Added more products in admin panel, and functionlity to product_info

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.every_product, name='products'),
+    path('<product_id>', views.product_info, name='product_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# Create your views here.
 
 def every_product(request):
-    """ A view to show all the products """
+    """ View that shows all products """
 
     products = Product.objects.all()
     context = {
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_info(request, product_id):
+    """ View to show one specified product and information"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_info.html', context)
