14eaaf05ac253678b4de4d0e988dc87257aa4219
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-06-18 13:50:14 +0000

    Added sizeto product models and options to choose size in product template

--- products/models.py
+++ products/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=200)
+    size = models.BooleanField(default=False, null=True, blank=True)
     writer = models.CharField(max_length=200, blank=True)
     description = models.TextField()
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
