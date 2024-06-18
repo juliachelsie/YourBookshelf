5192d6d99036c020e33a37ab2d2cb27061d996cc
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-06-18 11:13:18 +0000

    Updated shoppingbag template and fixed spelling mistake in contexts.py

--- shoppingbag/contexts.py
+++ shoppingbag/contexts.py
@@ -1,14 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def shoppingbag_contents(request):
 
     shoppingbag_items = []
     total = 0
     product_count = 0
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    for item_id, quantity in shoppingbag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        shoppingbag_items.append({
+            'item_id' : item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
