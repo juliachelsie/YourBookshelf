0d5492278a9fffdd23ba9940790690a80560daee
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-06-24 12:29:51 +0000

    Added remove and modify bag functionality

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_shoppingbag, name='view_shoppingbag'),
     path('add/<item_id>/', views.add_to_shoppingbag, name='add_to_shoppingbag'),
+    path('modify/<item_id>/', views.modify_shoppingbag, name='modify_shoppingbag'),
+    path('remove/<item_id>/', views.remove_from_shoppingbag, name='remove_from_shoppingbag'),
 ]
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_shoppingbag(request):
@@ -33,3 +33,50 @@ def add_to_shoppingbag(request, item_id):
     
     request.session['shoppingbag'] = shoppingbag
     return redirect(redirect_url)
+
+
+def modify_shoppingbag(request, item_id):
+    """ Modify the quantity of a product in the shoppingbag """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'p_size' in request.POST:
+        size = request.POST['p_size']    
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if size:
+        if quantity > 0:
+            shoppingbag[item_id]['items_by_size'][size] = quantity
+        else:
+            del shoppingbag[item_id]['items_by_size'][size]
+            if not shoppingbag[item_id]['items_by_size'][size]:
+                shoppingbag.pop(item_id)
+    else:
+        if quantity > 0:
+            shoppingbag[item_id] = quantity
+        else:
+            shoppingbag.pop(item_id)
+    
+    request.session['shoppingbag'] = shoppingbag
+    return redirect(reverse('view_shoppingbag'))
+
+
+def remove_from_shoppingbag(request, item_id):
+    """ Remove the quantity of a product from the shoppingbag """
+    try:
+        size = None
+        if 'p_size' in request.POST:
+            size = request.POST['p_size']    
+        shoppingbag = request.session.get('shoppingbag', {})
+
+        if size:
+                del shoppingbag[item_id]['items_by_size'][size]
+                if not shoppingbag[item_id]['items_by_size'][size]:
+                    shoppingbag.pop(item_id)
+        else:
+            shoppingbag.pop(item_id)
+        
+        request.session['shoppingbag'] = shoppingbag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
