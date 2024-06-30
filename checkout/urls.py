5769fa317317ef98e06035546a42d94bb620a071
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-06-30 19:46:20 +0000

    Webhook setup logic and exported wh secret to gitpod

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_win/<order_number>', views.checkout_win, name='checkout_win'),
+    path('wh/', webhook, name='webhook'),
 ]
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,7 +9,22 @@ class WH_Handler:
         """ Handles generic/unexpected/unknown webhook event """
 
         return HttpResponse(
+            content = f'Unhandled Webhook received: {event["type"]}',
+            status = 200
+        )
+
+    def handle_payment_succeeded(self, event):
+        """ Handles payment_intent.succeeded webhooks from Stripe """
+
+        return HttpResponse(
             content = f'Webhook received: {event["type"]}',
             status = 200
         )
-        
+
+    def handle_payment_failed(self, event):
+        """ Handles payment_intent.failed webhooks from Stripe """
+
+        return HttpResponse(
+            content = f'Webhook received: {event["type"]}',
+            status = 200
+        )
