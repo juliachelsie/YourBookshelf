c77f03bf27f1a8b46089574f04285e7c91f1adaa
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-07-11 15:50:35 +0000

    Deployment

--- yourbookshelf/settings.py
+++ yourbookshelf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-2s+w$*c#a1ze&(592i1n9vmv@i*y3va(c8jza9l94_kjriqhk-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-juliachelsi-yourbookshe-zg00vmr7oym.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['yourbookshelf.herokuapp.com', 'localhost', '8000-juliachelsi-yourbookshe-zg00vmr7oym.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -122,12 +123,24 @@ WSGI_APPLICATION = 'yourbookshelf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+ #     'default': {
+ #         'ENGINE': 'django.db.backends.sqlite3',
+ #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #     }
+ # }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
