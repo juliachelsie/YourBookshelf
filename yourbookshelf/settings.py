764a8fabad36eed5ab636b785147627dbde8b084
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-07-11 22:14:53 +0000

    Connected AWS in settings

--- yourbookshelf/settings.py
+++ yourbookshelf/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['yourbookshelf-f2ba8e75215b.herokuapp.com', 'localhost', '8000-juliachelsi-yourbookshe-zg00vmr7oym.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['yourbookshelf-f2ba8e75215b.herokuapp.com', 'localhost']
 
 
 # Application definition
