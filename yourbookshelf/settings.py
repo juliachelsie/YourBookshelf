c91771b4af2f1c737802459ac842126b99e8f6b1
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-07-12 08:32:49 +0000

    deployment

--- yourbookshelf/settings.py
+++ yourbookshelf/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://yourbookshelf-f2ba8e75215b.herokuapp.com', 'localhost']
 
