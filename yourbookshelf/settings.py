f014f6838e5352cbe08ff864a05743423453f89c
Julia Gustafsson <juliachelsiegustafsson@gmail.com>
2024-07-12 18:51:23 +0000

    Tried change debug mode

--- yourbookshelf/settings.py
+++ yourbookshelf/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-juliachelsi-yourbookshe-zg00vmr7oym.ws.codeinstitute-ide.net', 'https://yourbookshelf-f2ba8e75215b.herokuapp.com', 'yourbookshelf-f2ba8e75215b.herokuapp.com', 'localhost']
 
@@ -211,7 +211,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 40
